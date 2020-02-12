3bcee2edc8cfd2cacdc5dd625c49a76fd35937d1
MrMinemeet <alexander.voglsperger@gmail.com>
2020-02-12 06:46:12 +0100

    Update 11.2.2020

--- media/code_snippets/HTTPCamStream.py
+++ media/code_snippets/HTTPCamStream.py
@@ -8,6 +8,17 @@ import io, socketserver, picamera
 from threading import Condition
 from http import server
 
+
+# --- SETTINGS ---
+PORT = 5000
+RESOLUTION = '1280x720' # e.g 1920x1080, 1280x720, ...
+FPS = 30
+ROTATION = 180  # no rotation --> 0
+
+
+
+
+
 class streamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
@@ -31,21 +42,22 @@ class streamingOutput(object):
 class streamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
         # Check if user is requesting stream
-        if self.path == '/stream.mjpg': 
+        if self.path == '/stream.mjpg':
             # yes --> Send Stream
             # send header for beginning streaming
-            self.send_response(200) # OK
+            self.send_response(200) # HTTP OK
             self.send_header('Age', 0)
             self.send_header('Cache-Control', 'no-cache, private')
             self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace, boundary=FRAME')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
             self.end_headers()
 
-            # sending Stream
+            # start Stream
             try:
                 while True:
+                    # Send new image when available
                     with output.condition:
-                        output.condition.wait() # Wait for new image
+                        output.condition.wait()
                         frame = output.frame
                     # Header for sending image
                     self.wfile.write(b'--FRAME\r\n')
@@ -53,27 +65,26 @@ class streamingHandler(server.BaseHTTPRequestHandler):
                     self.send_header('Content-Length', len(frame))
                     self.end_headers()
                     self.wfile.write(frame)
-                    self.wfile.write(b'\r\n') # End transmission of frame
-            except Exception as ex:
-                print('Client removed ' + self.client_address + ' : ' + str(ex))
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                print('Removed streaming client %s: %s', self.client_address, str(e))
         else:
             # User requested something else --> Send 404 Page
             self.send_error(404)
             self.end_headers()
 
-
 # --- MAIN ---
 #Only resolution and framerate in Constructorparameters
-with picamera.PiCamera(resolution='1280x720', framerate=30) as cam:
+with picamera.PiCamera(resolution=RESOLUTION, framerate=FPS) as cam:
     output = streamingOutput()
     # Rotation if needed
-    cam.rotation = 180
+    cam.rotation = ROTATION
     #start stream
     cam.start_recording(output, format='mjpeg')
 
     try:
         #IP, Port
-        hostAndPort = ('',5000) # '' as IP automatically get's ip
+        hostAndPort = ('',PORT) # '' as IP automatically get's ip
         server = streamingServer(hostAndPort, streamingHandler)
         server.serve_forever() # Serves as long as script is running
     finally:
